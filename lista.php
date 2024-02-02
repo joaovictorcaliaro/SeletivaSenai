e3c84cf63993c30b7946d0ee105f225ab00fd107
João Victor Caliaro <jcaliaro231@gmail.com>
2024-02-02 15:52:48 -0300

    Criação do Home

---
+++ lista.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="pokedex.css">
+</head>
+
+<body class="pagina" id="lista">
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Pokédex</span>
+        </div>
+    </nav><br>
+    <div class="container" style="text-align: center;">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <img src="img_pokemons/Charmander.png" alt="" class="card-img-top">
+                    <div class="card-body"></div>
+                    <h5 class="card-title">
+                        Charmander
+                    </h5>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="img_pokemons/Bulbasur.png" alt="" class="card-img-top">
+                    <div class="card-body"></div>
+                    <h5 class="card-title">
+                        Bulbasaur
+                    </h5>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <img src="img_pokemons/Squirtle.png" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Squirtle
+                        </h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="img_pokemons/Caterpie.png" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Caterpie
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <img src="img_pokemons/Butterfree.png" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Butterfree
+                        </h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="img_pokemons/Weedle.png" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Weedle
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <img src="img_pokemons/Beedril.png" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Beedrill
+                        </h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="img_pokemons/Pidgey.png" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Pidgey
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <img src="img_pokemons/Rattata.png" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Rattata
+                        </h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="img_pokemons/Spearow.png" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Spearow
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <img src="img_pokemons/Ekans.png" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Ekans
+                        </h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="img_pokemons/Pikachu.png" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Pikachu
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div><br><br>
+        <footer style="width: 100%; background-color:white; text-align: center">
+            <h5>Desenvolvido por João Victor Caliaro</h5>
+        </footer>
+    </div>
+
+</body>
+
+</html>
